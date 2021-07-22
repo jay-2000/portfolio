c3daeea54722d7e9c9cd7bc06bfe0622898c374f
Jay Parmar <61630442+jay-2000@users.noreply.github.com>
2021-07-22 13:03:46 +0530

    Update up.js

--- up.js
+++ up.js
@@ -1,7 +1,6 @@
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -12,8 +11,6 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-//   document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.documentElement.scrollTop = 0;
 }
