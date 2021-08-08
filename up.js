a1d0e32538b9dc7596789e560694596f18ef8c35
Jay Parmar <61630442+jay-2000@users.noreply.github.com>
2021-08-09 00:46:47 +0530

    Add files via upload

--- up.js
+++ up.js
@@ -1,6 +1,7 @@
-
+//Get the button:
 mybutton = document.getElementById("myBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -11,6 +12,8 @@ function scrollFunction() {
   }
 }
 
+// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.documentElement.scrollTop = 0;
+//   document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
